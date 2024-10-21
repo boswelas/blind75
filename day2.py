bacb6ea8e1048ae4a534194549af0f3e786a184f
Ashley Boswell <boswelas@oregonstate.edu>
2024-10-21 16:02:42 -0700

    solved maxSubArray()

--- day2.py
+++ day2.py
@@ -3,5 +3,17 @@ from typing import List
 class Solution:
 
     def maxSubArray(self, nums: List[int]) -> int:
+        """Given an integer array nums, find the subarray with the largest sum, and return its sum."""
+        result = nums[0]
+        temp = 0
+        
+        for num in nums:
+            if temp < 0:
+                temp = 0
+            temp += num
+            result = max(result, temp)
+        return result
 
-        return
+solution = Solution()
+nums = [-2,-1]
+print(solution.maxSubArray(nums))
